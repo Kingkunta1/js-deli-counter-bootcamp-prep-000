7fa4bb8d3347e6f13fc463686977d2d77e128b6f
Kenton Archer <ken10archer@gmail.com>
2019-03-22 03:04:09 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -20,14 +20,21 @@ if(current.length === 0)
   }
 }
 
-
+//var curr= [ "bob", "jill", "bob", "jill"]
 function currentLine(current){
 if(current.length===0){  
    return "The line is currently empty."}
      
 else{  
     for( var i=0;i<current.length;i++)
-    return (`The line is currently: 1. ${current[0]}, 2. ${current[1]}, 3. ${current[2]}`)
+     //{`${i+1} ${current[i]}` }
+    
+    //how to iterate through each element?
+    return (`The line is currently: 
+    
+     1.${current[0]}, 2. ${current[1]}, 3. ${current[2]}`)
 
+//  return (`The line is currently: something else goes here)
+    
 }
   }
